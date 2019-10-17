0589bf676c294fad5ca9ba4b01b2868525430065
mmurtic2 <mmurtic2@etf.unsa.ba>
2019-10-17 16:43:21 +0200

    Implementacija metoda

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,66 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+
+    Artikl[] korpa = new Artikl[50];
+
+
+    public boolean dodajArtikl(Artikl a){
+
+        for(int i=0; i<50; i++){
+            if(korpa[i] == null){
+                korpa[i] = a;
+                break;
+            }
+        }
+
+        if(korpa.length <= 50){
+            return true;
+        }
+        return false;
+    }
+
+
+
+
+    public Artikl[] getArtikli() {
+
+        return korpa;
+    }
+
+    Artikl a;
+    public Artikl izbaciArtiklSaKodom(String kod){
+        Artikl[] pom = new Artikl[50];
+        for(int i=0; i<50; i++){
+            if(korpa[i].getKod().equals(kod) && i != 0) {
+                a = korpa[i];
+                System.arraycopy(korpa, 0, pom, 0, i-1);
+                System.arraycopy(korpa, i+1, pom, i, 50-i-1);
+                korpa = pom;
+                break;
+            }else if(korpa[i].getKod().equals(kod) && i == 0){
+                a = korpa[i];
+                System.arraycopy(korpa, 1, pom, 0, 49);
+                korpa = pom;
+                break;
+            }
+        }
+        return  a;
+    }
+
+
+
+    public int dajUkupnuCijenuArtikala() {
+        int ukupnaCijenaArtikala = 0;
+        for(Artikl artikl : korpa){
+            if(artikl != null) {
+                ukupnaCijenaArtikala = ukupnaCijenaArtikala + artikl.getCijena();
+            }
+            else{
+                break;
+            }
+        }
+        return  ukupnaCijenaArtikala;
+    }
 }
+
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,43 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+
+    Artikl[] artikli = new Artikl[1000];
+
+    public void dodajArtikl(Artikl a){
+        for(int i=0; i<1000; i++){
+            if(artikli[i] == null) {
+                artikli[i] = a;
+                break;
+            }
+        }
+    }
+
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    Artikl a;
+    public Artikl izbaciArtiklSaKodom(String kod){
+
+        Artikl[] pom = new Artikl[1000];
+        for(int i=0; i<1000; i++){
+            if(artikli[i].getKod().equals(kod) && i != 0) {
+                a = artikli[i];
+                System.arraycopy(artikli, 0, pom, 0, i-1);
+                System.arraycopy(artikli, i+1, pom, i, 1000-i-1);
+                artikli = pom;
+                break;
+            }else if(artikli[i].getKod().equals(kod) && i == 0){
+                a = artikli[i];
+                System.arraycopy(artikli, 1, pom, 0, 999);
+                artikli = pom;
+                break;
+            }
+        }
+        return a;
+    }
+
+
 }
